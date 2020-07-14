e32d7ff1b602085bf67f8cf7729fb1eb9037a122
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-14 18:42:47 +0700

    Implement quality decrease for conjured items

--- Item.cs
+++ Item.cs
@@ -14,13 +14,78 @@
         public void UpdateQuality()
         {
             if (Name != "Aged Brie" && Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Quality > 0)
                 {
-                    if (Quality > 0)
+                    if (Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (Name != "Sulfuras, Hand of Ragnaros")
+                        if (!Name.StartsWith("Conjured"))
                         {
                             Quality = Quality - 1;
                         }
+                        else
+                        {
+                            Quality = (Quality >= 2) ? Quality - 2 : 0;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Quality < 50)
+                {
+                    Quality = Quality + 1;
+
+                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (SellIn < 11)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality = Quality + 1;
+                            }
+                        }
+
+                        if (SellIn < 6)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality = Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Name != "Sulfuras, Hand of Ragnaros")
+            {
+                SellIn = SellIn - 1;
+            }
+
+            if (SellIn < 0)
+            {
+                if (Name != "Aged Brie")
+                {
+                    if (Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Quality > 0)
+                        {
+                            if (Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                if (!Name.StartsWith("Conjured"))
+                                {
+                                    Quality = Quality - 1;
+                                }
+                                else
+                                {
+                                    Quality = Quality - 2;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Quality = Quality - Quality;
                     }
                 }
                 else
@@ -28,60 +93,9 @@
                     if (Quality < 50)
                     {
                         Quality = Quality + 1;
-
-                        if (Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (SellIn < 11)
-                            {
-                                if (Quality < 50)
-                                {
-                                    Quality = Quality + 1;
-                                }
-                            }
-
-                            if (SellIn < 6)
-                            {
-                                if (Quality < 50)
-                                {
-                                    Quality = Quality + 1;
-                                }
-                            }
-                        }
                     }
                 }
-
-                if (Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    SellIn = SellIn - 1;
-                }
-
-                if (SellIn < 0)
-                {
-                    if (Name != "Aged Brie")
-                    {
-                        if (Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Quality > 0)
-                            {
-                                if (Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Quality = Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Quality = Quality - Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Quality < 50)
-                        {
-                            Quality = Quality + 1;
-                        }
-                    }
-                }
+            }
         }
     }
 }

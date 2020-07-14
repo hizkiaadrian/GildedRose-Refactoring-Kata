52f6d048371b0200059efc42f1da7b7f646d3ed7
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-14 18:29:03 +0700

    Unit test for all items up to conjured items

---
+++ GildedRoseTests/AgedBrieTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using csharp;
+using NUnit.Framework;
+
+namespace GildedRoseTests
+{
+    public class AgedBrieTest
+    {
+        [Test]
+        public void AgedBrie_QualityIncreasesBeforeSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            
+            Assert.AreEqual(3, Items[0].Quality);
+        }
+        
+        [Test]
+        public void AgedBrie_QualityIncreasesByTwoAfterSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            
+            Assert.AreEqual(6, Items[0].Quality);
+        }
+        
+        [Test]
+        public void AgedBrie_QualityNeverMoreThanFifty()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 49 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+    }
+}

e47bfe86c067d90b82cce1005c05d61eea464ce7
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-14 17:57:57 +0700

    Aged brie test

---
+++ AgedBrieTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class AgedBrieTest
+    {
+        [Test]
+        public void AgedBrie_QualityIncreasesBeforeSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            
+            Assert.AreEqual(3, Items[0].Quality);
+        }
+        
+        [Test]
+        public void AgedBrie_QualityIncreasesByTwoAfterSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            
+            Assert.AreEqual(6, Items[0].Quality);
+        }
+    }
+}
